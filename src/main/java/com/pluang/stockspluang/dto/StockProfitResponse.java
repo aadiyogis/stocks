67b18801fcd01ed3b50c2b81177582485ba618ba
aditya <adityaajmera89@gmail.com>
2022-02-12 18:01:18 +0530

    fixing null values for response.

--- src/main/java/com/pluang/stockspluang/dto/StockProfitResponse.java
+++ src/main/java/com/pluang/stockspluang/dto/StockProfitResponse.java
@@ -1,9 +1,11 @@
 package com.pluang.stockspluang.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StockProfitResponse {
 
     @JsonProperty("Strategy")
