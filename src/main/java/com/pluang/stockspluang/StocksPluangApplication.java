f22536944cf4ea3227ce2a69aa4f32f398164339
aditya <adityaajmera89@gmail.com>
2022-02-12 13:12:33 +0530

    initial project setup

---
+++ src/main/java/com/pluang/stockspluang/StocksPluangApplication.java
@@ -0,0 +1,13 @@
+package com.pluang.stockspluang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksPluangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksPluangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pluang/stockspluang/StocksPluangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluang.stockspluang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksPluangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
